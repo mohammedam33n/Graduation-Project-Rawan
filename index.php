4504eadeeb8770bef199719a4f813513090a1306
mohammedam33n <mohammedam33n@gmail.com>
2024-05-17 05:10:21 +0300

    init

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home Page</title>
+    <link rel="stylesheet" href="home.css">
+</head>
+
+<body>
+    <header>
+        <!-- Navigation menu -->
+    </header>
+
+    <main>
+        <div class="slider">
+            <!-- Slider Ads -->
+        </div>
+
+        <div class="trips">
+            <!-- Available trips -->
+        </div>
+
+        <div class="icons">
+            <a href="show_profile.html">Profile</a>
+            <a href="#">Notification</a>
+            <a href="#">My trips</a>
+        </div>
+    </main>
+
+    <footer>
+        <!-- Footer content -->
+    </footer>
+
+    <script src="./assets/js/"></script>
+</body>
+
+</html>
